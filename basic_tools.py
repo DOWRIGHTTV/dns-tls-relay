38c042b37818dd8a218bdc499fbf611e280076af
DOWRIGHT <dowright@nationofnobodies.com>
2021-07-31 15:32:42 -0700

    removed unneeded function

--- basic_tools.py
+++ basic_tools.py
@@ -101,21 +101,6 @@ def write_cache(top_domains):
     with open('top_domains.json', 'w') as cache:
         json.dump(f_cache, cache, indent=4)
 
-def record_parse_error(log_info):
-    info = SimpleNamespace(**log_info)
-
-    print('+'*30)
-    print(f'UNSEEN RECORD TYPE :/ | {info.rtype}')
-    print(f'NAME LENGTH: {info.nlen}')
-    print(info.data)
-    print('='*30)
-    with open('dns_tls_relay.error', 'a+') as errors:
-        errors.write('++++++++++++++++++++++++++\n')
-        errors.write(f'UNSEEN RECORD TYPE :/ | {info.rtype}\n')
-        errors.write(f'NAME LENGTH: {info.nlen}\n')
-        errors.write(f'{info.data}\n')
-        errors.write('==========================\n')
-
 def looper(sleep_len):
     def decorator(loop_function):
         def wrapper(*args):
